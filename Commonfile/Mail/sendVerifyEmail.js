05c47386f1c01962bb40e2c1628e0fc6dc8ff08a
Iqbalhossen <arfinqibal8@gmail.com>
2023-02-02 01:26:38 +0600

    update

--- Commonfile/Mail/sendVerifyEmail.js
+++ Commonfile/Mail/sendVerifyEmail.js
@@ -6,8 +6,8 @@ const sendVerifyEmail = async (name, email, user_id) => {
 
         const transpoter = nodemailer.createTransport({
 
-            host: 'sendmail.gmail.com',
-            port: 587,
+            port: 465,
+            host: "smtp.gmail.com",
             secure: false,
             requireTLS: true,
             auth: {
@@ -20,14 +20,14 @@ const sendVerifyEmail = async (name, email, user_id) => {
             from: 'miller5547l@gmail.com',
             to: email,
             subject: 'For Acoount Verficition in Yume One',
-            html: '<h3> Hi! ' + name + '</h3>' + ' <p> Please Click Here <a href="https://animated-chaja-d2e0e0.netlify.app/verify/account/'+user_id + user_id +'/'+ user_id +'"> Verify </a></p>'
+            html: '<h3> Hi! ' + name + '</h3>' + ' <p> Please Click Here <a href="https://animated-chaja-d2e0e0.netlify.app/verify/account/' + user_id + user_id + '/' + user_id + '"> Verify </a></p>'
         }
 
-        transpoter.sendMail(mailOption, function (error,info) {
-            if(error){
+        transpoter.sendMail(mailOption, function (error, info) {
+            if (error) {
                 console.log(error);
-            }else{
-                console.log('email send',info.response);
+            } else {
+                console.log('email send', info.response);
             }
         })
 
